2ee715950294592b0e5c40b3e855d62aa1ff71f8
mikhaelapg <mikhaelapg00@gmail.com>
2023-06-30 07:46:11 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+let js = 'amazing';
+// if (js === 'amazing') alert('JavaScript is FUN!');
+// if still want to display explicit ouput to result use console.log(40 + 8 + 23 - 10);
+console.log(40 + 8 + 23 - 10);
+
+// if still want to display explicit ouput to result use console.log(40 + 8 + 23 - 10); 
+console.log(40 + 8 + 23 - 10);
+console.log("Jonas");
+console.log(23);
+
+// variable and values
+let firstName = "matilda";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let jonas_matilda = "JM";
+let $function = 27;
+
+let Person = "jonas";
+let PI = 3.1415;
+
+let job1 = "programmer";
+let job2 = "teacher";
+
+// do this
+let myFirstJob = "Coder";
+let myCurrentJob = "Teacher";
+let person = "jonas";
+
+console.log(myFirstJob);
