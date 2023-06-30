14825593f5ab3c10a825779c4988bf69c0894d93
mikhaelapg <mikhaelapg00@gmail.com>
2023-06-30 10:26:35 +0700

    Compare BMI

--- script.js
+++ script.js
@@ -127,3 +127,18 @@ console.log(x, y);
 
 const averageAge = (ageJonas + ageSarah) / 2;
 console.log(ageJonas, ageSarah, averageAge);
+
+// massMark = 78;
+// heightMark = 1.69;
+// massJohn = 92;
+// heightJohn = 1.95;
+massMark = 95;
+heightMark = 1.88;
+massJohn = 85;
+heightJohn = 1.76;
+
+BMIMark = massMark / heightMark ** 2;
+BMIJohn = massJohn / (heightJohn * heightJohn);
+
+markHigherBMI = BMIMark > BMIJohn;
+console.log(BMIMark, BMIJohn, markHigherBMI);
